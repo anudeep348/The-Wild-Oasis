39a625fb4ca053e20c9bda12541b5356db6c6475
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-01-16 10:10:07 +0530

    completed till Toast Notifications committing due to laptop issue

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://evumqwzpvvkhqrjltdov.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2dW1xd3pwdnZraHFyamx0ZG92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ1MzUxMzAsImV4cCI6MjAyMDExMTEzMH0.2PPHJsDQGitE7MihxvWY67IGdpmHXJGitZUNVNQgeyA";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
---
+++ src/ui/Input.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  border: 1px solid var(--color-grey-300);
+  background-color: var(--color-grey-0);
+  border-radius: var(--border-radius-sm);
+  box-shadow: var(--shadow-sm);
+
+  padding: 0.8rem 1.2rem;
+  outline: none;
+
+  &:hover,
+  &:focus {
+    outline: 1px solid #785687;
+    outline-offset: 1px;
+  }
+`;
+
+export default Input;
