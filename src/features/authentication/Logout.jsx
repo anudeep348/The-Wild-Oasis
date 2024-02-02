77d59d20e077b5ad87f626d662747efe4d026444
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-02-02 17:04:18 +0530

    added new fetures like user Logout and fully functionl signUp form and allowd user to update their username,avatar  and password

---
+++ src/features/authentication/Logout.jsx
@@ -0,0 +1,19 @@
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+import ButtonIcon from "../../ui/ButtonIcon";
+import { useLogout } from "./useLogout";
+import Spinner from "../../ui/Spinner";
+import SpinnerMini from "../../ui/SpinnerMini";
+
+function Logout() {
+  const { logout, isLoading } = useLogout();
+
+  if (isLoading) return <Spinner />;
+
+  return (
+    <ButtonIcon disabled={isLoading} onClick={logout}>
+      {!isLoading ? <HiArrowRightOnRectangle /> : <SpinnerMini />}
+    </ButtonIcon>
+  );
+}
+
+export default Logout;
